aaf90aa8343d1247c451629536f376fe7b5d38c7
shubhampandya7642 <93577967+shubhampandya7642@users.noreply.github.com>
2024-07-05 17:17:41 +0530

    Update dice.js
    Improved code execution

--- dice.js
+++ dice.js
@@ -4,19 +4,12 @@ const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
 const h2 = document.getElementById("h2");
 
-const rand = ()=>{
-    return Math.round(Math.random() * (5) + 1);
-}
-
 rollButton.onclick = ()=>{
-    console.log("Button clicked!!");
-    const num1 = rand();
-    const num2 = rand();
-    console.log("Num1: "+num1+" Num2: "+num2);
-    const src1 = "images/"+num1+".png";
-    const src2 = "images/"+num2+".png";
-    img1.src = src1;
-    img2.src = src2;
+    const num1 = Math.round(Math.random() * (5) + 1);    //Generating a random number between 1 and 6
+    const num2 = Math.round(Math.random() * (5) + 1);    //Generating a random number between 1 and 6
+    console.log("Num1: "+num1+" Num2: "+num2);    //Just for verification
+    img1.src = num1+".png";
+    img2.src = num2+".png";
 
     if(num1 > num2){
         h2.innerText = winStatementArray[0];
